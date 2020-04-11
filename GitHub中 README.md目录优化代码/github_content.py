995073e82e265171facfa70a478eb65f6b0236ea
Jason-pro1 <56431243+Jason-pro1@users.noreply.github.com>
2020-04-11 22:38:17 +0800

    Update github_content.py

--- GitHub中 README.md目录优化代码/github_content.py
+++ GitHub中 README.md目录优化代码/github_content.py
@@ -1,4 +1,7 @@
 def github_content(content1, head, tail):
+    """在README中目录不能自动换行，在编辑目录时，
+    需要在每一行后面添加一个tab(四个空格)，
+    再一个enter才能做到真正换行"""
     count_1 = count_2 = 0
     content = content1.replace(tail, "    ")
     n = len(content)
